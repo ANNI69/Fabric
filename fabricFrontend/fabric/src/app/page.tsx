15c3de913a2305d84f7bb379566e6d586631250c
Anii <aniruddh.nagare@vpt.edu.in>
2024-02-25 02:51:12 +0530

    dark-light

--- fabricFrontend/fabric/src/app/page.tsx
+++ fabricFrontend/fabric/src/app/page.tsx
@@ -1,11 +1,134 @@
+"use client";
 import Image from "next/image";
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { LayoutGrid } from "@/components/ui/layout-grid";
 
 export default function Home() {
+  const { setTheme } = useTheme();
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  const cards = [
+    {
+      id: 1,
+      content: <SkeletonOne />,
+      className: "md:col-span-2",
+      thumbnail:
+        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
+    },
+    {
+      id: 2,
+      content: <SkeletonTwo />,
+      className: "col-span-1",
+      thumbnail:
+        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
+    },
+    {
+      id: 3,
+      content: <SkeletonThree />,
+      className: "col-span-1",
+      thumbnail:
+        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
+    },
+    {
+      id: 4,
+      content: <SkeletonFour />,
+      className: "md:col-span-2",
+      thumbnail:
+        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
+    },
+  ];
+  
+
   return (
     <>
-      <div id="font-bold">
-        <h1>Home</h1>
-      </div> 
+      <div className="relative  w-full">
+        <FloatingNav navItems={navItems} />
+        <div className="bg-zinc-950 dark:bg-black" />
+        <div className="relative  w-full"></div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+          <h1 className="text-6xl font-mono"></h1>
+          <div className="flex space-x-4">
+            <button onClick={() => setTheme("light")}>Light</button>
+            <button onClick={() => setTheme("dark")}>Dark</button>
+          </div>
+        </div>
+        <div className="relative m-10 p-30">
+          <div className="h-screen py-20 w-full">
+            
+          </div>
+        </div>
+      </div>
     </>
   );
 }
+
+const SkeletonOne = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">House in the woods</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A serene and tranquil retreat, this house in the woods offers a peaceful
+        escape from the hustle and bustle of city life.
+      </p>
+    </div>
+  );
+};
+
+const SkeletonTwo = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">House above the clouds</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        Perched high above the world, this house offers breathtaking views and a
+        unique living experience. It&apos;s a place where the sky meets home,
+        and tranquility is a way of life.
+      </p>
+    </div>
+  );
+};
+const SkeletonThree = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Greens all over</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
+        perfect place to relax, unwind, and enjoy life.
+      </p>
+    </div>
+  );
+};
+const SkeletonFour = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Rivers are serene</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A house by the river is a place of peace and tranquility. It&apos;s the
+        perfect place to relax, unwind, and enjoy life.
+      </p>
+    </div>
+  );
+};
