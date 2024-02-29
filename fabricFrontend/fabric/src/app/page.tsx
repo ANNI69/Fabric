4fd2d441739904d71db4a16332ae4c09c526fc7b
Anii <aniruddh.nagare@vpt.edu.in>
2024-03-01 01:07:41 +0530

    Home

--- fabricFrontend/fabric/src/app/page.tsx
+++ fabricFrontend/fabric/src/app/page.tsx
@@ -3,11 +3,33 @@ import Image from "next/image";
 import * as React from "react";
 import { useTheme } from "next-themes";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import {
+  IconHome,
+  IconMessage,
+  IconShirt,
+  IconShoppingBag,
+  IconUser,
+} from "@tabler/icons-react";
 import { LayoutGrid } from "@/components/ui/layout-grid";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SunIcon, MoonIcon } from "lucide-react";
 
 export default function Home() {
-  const { setTheme } = useTheme();
+  
   const navItems = [
     {
       name: "Home",
@@ -15,117 +37,57 @@ export default function Home() {
       icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
-      name: "About",
-      link: "/about",
-      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      name: "Shop",
+      link: "/shop",
+      icon: <IconShirt className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
-      name: "Contact",
-      link: "/contact",
+      name: "Cart",
+      link: "/cart",
       icon: (
-        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+        <IconShoppingBag className="h-4 w-4 text-neutral-500 dark:text-white" />
       ),
     },
   ];
-  const cards = [
-    {
-      id: 1,
-      content: <SkeletonOne />,
-      className: "md:col-span-2",
-      thumbnail:
-        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
-    },
-    {
-      id: 2,
-      content: <SkeletonTwo />,
-      className: "col-span-1",
-      thumbnail:
-        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
-    },
-    {
-      id: 3,
-      content: <SkeletonThree />,
-      className: "col-span-1",
-      thumbnail:
-        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
-    },
-    {
-      id: 4,
-      content: <SkeletonFour />,
-      className: "md:col-span-2",
-      thumbnail:
-        "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
-    },
+  const imageSrc = [
+    "https://i.pinimg.com/564x/4b/00/18/4b0018f3d92dbd63f4062420e2e074d4.jpg",
+    "https://i.pinimg.com/564x/c7/2f/f3/c72ff3943e92090ed98b6d9f30d1f1c1.jpg",
+    "https://i.pinimg.com/564x/dc/31/6a/dc316a70341d7f311fb1e9a66dd949ab.jpg",
+    "https://i.pinimg.com/564x/96/41/2b/96412b918138e376c47c832f18fb5056.jpg",
+    "https://i.pinimg.com/564x/bf/7d/b3/bf7db3a2f01963bb6fe7f6726f95b253.jpg",
   ];
 
   return (
     <>
-      <div className="relative  w-full">
+      <div className="relative mt-40 w-full">
         <FloatingNav navItems={navItems} />
-        <div className="bg-zinc-950 dark:bg-black" />
-        <div className="relative  w-full"></div>
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-          <h1 className="text-6xl font-mono"></h1>
-          <div className="flex space-x-4 font-['Milk']">
-            <button onClick={() => setTheme("light")}>Light</button>
-            <button onClick={() => setTheme("dark")}>Dark</button>
-          </div>
-        </div>
-        <div className="relative m-10 p-30">
-          <div className="h-screen py-20 w-full"></div>
+        <div className="relative flex justify-center m-10 p-30">
+          <Carousel className="w-full max-w-xs ">
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <Image
+                          src={imageSrc[index]}
+                          alt={"Image"}
+                          width={300}
+                          height={300}
+                          objectFit="cover"
+                          className="rounded-lg"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </div>
     </>
   );
 }
-
-const SkeletonOne = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">House in the woods</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        A serene and tranquil retreat, this house in the woods offers a peaceful
-        escape from the hustle and bustle of city life.
-      </p>
-    </div>
-  );
-};
-
-const SkeletonTwo = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">House above the clouds</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Perched high above the world, this house offers breathtaking views and a
-        unique living experience. It&apos;s a place where the sky meets home,
-        and tranquility is a way of life.
-      </p>
-    </div>
-  );
-};
-const SkeletonThree = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">Greens all over</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
-      </p>
-    </div>
-  );
-};
-const SkeletonFour = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">Rivers are serene</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        A house by the river is a place of peace and tranquility. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
-      </p>
-    </div>
-  );
-};
